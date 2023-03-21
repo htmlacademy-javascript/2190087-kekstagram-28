cf9fee14aa671b89c2350722ca9bef3a807b69c9
Anna Amirova <anna.a.amirova@gmail.com>
2023-03-21 20:46:25 +0300

    Отрисовывет комментарии

--- js/fullscreen-photo.js
+++ js/fullscreen-photo.js
@@ -1,10 +1,47 @@
 import {isEscapeKey} from './util.js';
 
+const COMMENTS_TO_SHOW = 5;
+let commentsShown = 0;
+
 const body = document.querySelector('body');
 const fullscreenPhoto = document.querySelector('.big-picture');
 const commentsLoader = document.querySelector('.comments-loader');
 const commentCount = document.querySelector('.social__comment-count');
 const closeButton = document.querySelector('.big-picture__cancel');
+const commentsList = document.querySelector('.social__comments');
+
+//Отрисовывет комментарии
+const createComment = ({ avatar, name, message }) => {
+  const comment = document.createElement('li');
+  comment.innerHTML = '<img class="social__picture" src="" alt="" width="35" height="35">';
+  comment.classList.add('social__comment');
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+};
+
+const renderComments = (comments) => {
+  commentsShown += COMMENTS_TO_SHOW;
+
+  if (commentsShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const commentElement = createComment(comments[i]);
+    fragment.append(commentElement);
+  }
+  commentsList.innerHTML = '';
+  commentsList.append(fragment);
+  commentCount.innerHTML = `${commentsShown.length} из <span class="commnts-count">${comments.length}</span>комментариев`;
+};
+
+const onCommentsLoaderClick = () => renderComments();
 
 //Отрисовывет фото
 export const renderFullPhoto = ({url, description, likes}) => {
@@ -28,6 +65,7 @@ const hideFullscreenPhoto = () => {
   body.classList.remove('modal-open');
   closeButton.removeEventListener('click', hideFullscreenPhoto);
   document.removeEventListener('keydown', onEscape);
+  commentsShown = 0;
 };
 
 //Открывет фото
@@ -40,4 +78,7 @@ export const showFullscreenPhoto = (data) => {
   document.addEventListener('keydown', onEscape);
 
   renderFullPhoto(data);
+  renderComments(data);
+  commentsLoader.addEventListener('click', onCommentsLoaderClick);
 };
+

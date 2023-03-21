becab41e5ad8b172afd483bdd1d2cc7b4b712d48
Anna Amirova <anna.a.amirova@gmail.com>
2023-03-21 10:58:10 +0300

    Закрывает окно по клику и esc

--- js/fullscreen-photo.js
+++ js/fullscreen-photo.js
@@ -1,8 +1,12 @@
+import {isEscapeKey} from './util.js';
+
 const body = document.querySelector('body');
 const fullscreenPhoto = document.querySelector('.big-picture');
 const commentsLoader = document.querySelector('.comments-loader');
 const commentCount = document.querySelector('.social__comment-count');
+const closeButton = document.querySelector('.big-picture__cancel');
 
+//Отрисовывет фото
 export const renderFullPhoto = ({url, description, likes}) => {
   fullscreenPhoto.querySelector('.big-picture__img img').src = url;
   fullscreenPhoto.querySelector('.likes-count').textContent = likes;
@@ -10,11 +14,30 @@ export const renderFullPhoto = ({url, description, likes}) => {
   fullscreenPhoto.querySelector('.big-picture__img img').alt = description;
 };
 
+//Закрывает фото
+const onEscape = (evt) => {
+  if (isEscapeKey (evt)) {
+    evt.preventDefault();
+    fullscreenPhoto.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
+};
+
+const hideFullscreenPhoto = () => {
+  fullscreenPhoto.classList.add('hidden');
+  body.classList.remove('modal-open');
+  closeButton.removeEventListener('click', hideFullscreenPhoto);
+  document.removeEventListener('keydown', onEscape);
+};
+
+//Открывет фото
 export const showFullscreenPhoto = (data) => {
   fullscreenPhoto.classList.remove('hidden');
   body.classList.add('modal-open');
   commentsLoader.classList.add('hidden');
   commentCount.classList.add('hidden');
+  closeButton.addEventListener('click', hideFullscreenPhoto);
+  document.addEventListener('keydown', onEscape);
 
   renderFullPhoto(data);
 };
